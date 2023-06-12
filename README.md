0db7e0534689b3fc65458b4fcfff234c35be2e23
shaimeur <76816609+shaimeur@users.noreply.github.com>
2023-06-12 14:33:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bam-test-full-stack-geevrp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bam-test-full-stack-o7nhht)
---
+++ index.js
@@ -0,0 +1,27 @@
+// Import stylesheets
+import './helpers/style.css';
+import * as helper from './helpers/helpers.js';
+
+// VOTRE NOM ET PRENOM ICI !
+
+
+function solutionExercice1(N) {
+    var res = "EXERCICE 1"
+    //TAPE LE CODE DE L'EXERCICE 1 ICI!
+    return res
+}
+
+function solutionExercice2(L) {
+    var res = "EXERCICE 2"
+    //TAPE LE CODE DE L'EXERCICE 2 ICI!
+    return res
+}
+function solutionExercice3(L) {
+    var res = "EXERCICE 3"
+    //TAPE LE CODE DE L'EXERCICE 3 ICI!
+    return res
+}
+
+helper.displayOnHtml(solutionExercice1(1000),'1')
+helper.displayOnHtml(solutionExercice2([1,4,7,5,3,6,3]),'2')
+helper.displayOnHtml(solutionExercice3([1,5,3,6,4,3,1,5,4]),'3')
